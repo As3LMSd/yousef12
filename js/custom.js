4aa655528fa0ad2b5cc5631f720fd016fa9cfd87
As3LMSd <y0567972529@gmail.com>
2025-06-05 14:52:11 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,21 @@ $(function(){
 
 
 
-// $(window).on("scroll",function(){
-//     console.log($(window).scrollTop());
-// });
+$(window).on("scroll",function(){
+    console.log($(window).scrollTop());
+});
 
 // $(window).on("scroll",function(){
 //     if($(window).scrollTop()>=100){
 //         $(".top").animate({
 //             opacity:"1"
 //         },500)
-//         else{
-//             $(".top").animate({
-//                 opacity:"0"
-//             },500)
-//         }
+
+//     }
+//     else{
+//         $(".top").animate({
+//             opacity:"0"
+//         },500)
 //     }
 // })
 
@@ -26,13 +27,15 @@ $(function(){
 // })
 
 
-// $('.js-example-basic-single').select2();
-// $(".bars").on("click",function(){
-// $(".bars").hide(".b")
-// })
+
+
+
+
+
 
 
 })
+
 
 $(window).on("load",function(){
     $(".lds-hourglass").fadeOut(500,function(){
